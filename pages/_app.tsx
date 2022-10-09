c9b4ef6a54ee4c9767fef1bc240325fe4932d4d4
Denis <bessonov94@gmail.com>
2022-10-09 18:01:12 +0300

    simple components - Button, Htag, Ptag, Tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
+import Head from 'next/head';
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>MyTop - наш лучший топ</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
