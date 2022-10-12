627ee652d406f2599ae0c508bae66c8d64919d1d
denis <denis@edu.yar.ru>
2022-10-12 18:57:02 +0300

    higher order component Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { Button, Htag, Ptag, Rating, Tag } from "../components";
+import { withLayout } from "../layout/Layout";
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
@@ -21,3 +22,5 @@ export default function Home(): JSX.Element {
     </>
   );
 }
+
+export default withLayout(Home);
