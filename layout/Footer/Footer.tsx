bd4d04c8c420c54ced4aa626428f21d807e744f8
Denis <bessonov94@gmail.com>
2022-10-16 14:00:21 +0300

    Layout, Footer grid styles

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,12 +1,15 @@
 import { FooterProps } from "./Footer.props";
 import styles from "./Footer.module.css";
+import { format } from 'date-fns';
 import cn from 'classnames';
 
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
   return (
-    <div {...props}>
-      Footer
-    </div>
+    <footer className={cn(className, styles.footer)} {...props}>
+        <div className={styles.corp}>OwlTop @ 2020 - {format(new Date(), 'yyyy')} все права защищены</div>
+        <a href="#" target="_blank" className={styles.agreement}>Пользовательское соглашение</a>
+        <a href="#" target="_blank" className={styles.politics}>Политика конфиденциальности</a>
+    </footer>
   );
 };
