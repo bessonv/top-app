dc46d7152a2e5ca2e106ae8fdf2e1d0339f5b60f
Denis <bessonov94@gmail.com>
2022-11-12 14:44:35 +0300

    reducer bug

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -3,7 +3,7 @@ import { TopPageComponentProps } from "./TopPageComponent.props";
 import styles from './TopPageComponent.module.css';
 import { TopLevelCategory } from "../../interfaces/page.interface";
 import { SortEnum } from "../../components/Sort/Sort.props";
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { sortReducer } from "./sort.reducer";
 
 export const TopPageComponent = ({ page, products, firstCategory}: TopPageComponentProps): JSX.Element => {
@@ -12,6 +12,10 @@ export const TopPageComponent = ({ page, products, firstCategory}: TopPageCompon
   const setSort = (sort: SortEnum) => {
     dispatchSort({ type: sort});
   };
+
+  useEffect(() => {
+    dispatchSort({ type: 'reset', initialState: products });
+  }, [products]);
 
   return (
     <div className={styles.wrapper}>
