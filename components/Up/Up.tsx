ff70de8bfb6995d9aa71555a56bfe894be236411
Denis <bessonov94@gmail.com>
2022-12-03 13:05:28 +0300

    component ButtonIcon

--- components/Up/Up.tsx
+++ components/Up/Up.tsx
@@ -1,9 +1,9 @@
 import { useAnimation } from "framer-motion";
 import { useScrollY } from "../../hooks/useScrollY";
 import styles from "./Up.module.css";
-import UpIcon from './up.svg';
 import { motion } from "framer-motion";
 import { useEffect } from "react";
+import { ButtonIcon } from "../ButtonIcon/ButtonIcon";
 
 export const Up = (): JSX.Element => {
   const controls = useAnimation();
@@ -21,13 +21,12 @@ export const Up = (): JSX.Element => {
   };
 
   return (
-    <motion.button 
+    <motion.div 
       className={styles.up} 
-      onClick={scrolltoTop}
       animate={controls}
       initial={{ opacity: 0 }}
     >
-      <UpIcon />
-    </motion.button>
+      <ButtonIcon appearance='primary' icon='up' onClick={scrolltoTop}/>
+    </motion.div>
   );
 };
