3c05a05dfe207fb915303c4257304d787a8e9af7
Denis <bessonov94@gmail.com>
2022-12-03 13:44:07 +0300

    button hover animation

--- components/Button/Button.props.ts
+++ components/Button/Button.props.ts
@@ -1,6 +1,8 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
 
-export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+export interface ButtonProps extends 
+Omit<DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>,
+'onAnimationStart' | 'onDragStart' | 'onDragEnd' | 'onDrag' | 'ref'> {
   //DetailedHTMLProps<E<T>, T>
   children: ReactNode;
   appearance: 'primary' | 'ghost';
