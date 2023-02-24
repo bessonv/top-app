d5a0826293f1627ad3ef93d2d91d14cc52b4e1f3
Denis <bessonov94@gmail.com>
2023-02-24 16:53:30 +0300

    useReducedMotion hook

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -5,9 +5,11 @@ import { TopLevelCategory } from "../../interfaces/page.interface";
 import { SortEnum } from "../../components/Sort/Sort.props";
 import { useEffect, useReducer } from "react";
 import { sortReducer } from "./sort.reducer";
+import { useReducedMotion } from "framer-motion";
 
 export const TopPageComponent = ({ page, products, firstCategory}: TopPageComponentProps): JSX.Element => {
   const [{ products: sortedProducts, sort }, dispatchSort] = useReducer(sortReducer, { products, sort: SortEnum.Rating});
+  const shouldReduceMotion = useReducedMotion();
 
   const setSort = (sort: SortEnum) => {
     dispatchSort({ type: sort});
@@ -25,7 +27,7 @@ export const TopPageComponent = ({ page, products, firstCategory}: TopPageCompon
         <Sort sort={sort} setSort={setSort}/>
       </div>
       <div role="list">
-        {sortedProducts && sortedProducts.map(p => (<Product layout key={p._id} product={p} role="listitem"/>))}
+        {sortedProducts && sortedProducts.map(p => (<Product layout={shouldReduceMotion ? false : true} key={p._id} product={p} role="listitem"/>))}
       </div>
       <div className={styles.hhTitle}>
         <Htag tag='h2'>Вакансии - {page.category}</Htag>
