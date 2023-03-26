f3b7304680451f1fd418339e14fc70d1d0e35e54
Denis <bessonov94@gmail.com>
2023-03-26 13:19:52 +0300

    meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,15 @@ import Head from 'next/head';
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   return <>
     <Head>
       <title>MyTop - наш лучший топ</title>
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
+      <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}/>
+      <meta property="og:locale" content="ru_RU"/>
     </Head>
     <Component {...pageProps} />
   </>;
