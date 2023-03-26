e2c73dceecd400e6ba3b9081eff00e882ba84ae6
Denis <bessonov94@gmail.com>
2023-03-26 13:41:26 +0300

    yandex metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,31 @@
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
+import ym from 'react-yandex-metrika';
+import { YMInitializer } from 'react-yandex-metrika';
+import Router from 'next/router';
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
+  Router.events.on('routeChangeComplete', (url: string) => {
+    if (typeof window !== 'undefined') {
+      ym('hit', url);
+    }
+  });
+  
   return <>
     <Head>
       <title>MyTop - наш лучший топ</title>
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://mc.yandex.ru" />
       <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
       <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}/>
       <meta property="og:locale" content="ru_RU"/>
     </Head>
+    <YMInitializer
+      accounts={[]}
+      options={{ webvisor: true, defer: true }}
+    />
     <Component {...pageProps} />
   </>;
 }
